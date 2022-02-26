a3359d28e964dfac33163ee49ac6d392ef8f7c5f
eabasquliyev <elgun.abasquliyev2@gmail.com>
2022-02-26 22:20:14 +0400

    Add IDepartment

---
+++ VacationPortal.DataAccess/Repositories/Abstracts/IDepartmentRepository.cs
@@ -0,0 +1,9 @@
+﻿using VacationPortal.Models;
+
+namespace VacationPortal.DataAccess.Repositories.Abstracts
+{
+    public interface IDepartmentRepository: IRepository<Department>
+    {
+        void Update(Department entity);
+    } 
+}
