ab9b24af97c436cd614df4a0c9c765aaca70f279
eabasquliyev <elgun.abasquliyev2@gmail.com>
2022-02-26 22:55:27 +0400

    Add IPositionRepository interface

---
+++ VacationPortal.DataAccess/Repositories/Abstracts/IPositionRepository.cs
@@ -0,0 +1,9 @@
+﻿using VacationPortal.Models;
+
+namespace VacationPortal.DataAccess.Repositories.Abstracts
+{
+    public interface IPositionRepository : IRepository<Position>
+    {
+        void Update(Position entity);
+    } 
+}
