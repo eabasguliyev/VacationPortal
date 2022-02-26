3f9b43b4a9b4aaeac0af15b9d718a593c93a7e1b
eabasquliyev <elgun.abasquliyev2@gmail.com>
2022-02-26 21:41:02 +0400

    Add IRepository

---
+++ VacationPortal.DataAccess/Repositories/Abstracts/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace VacationPortal.DataAccess.Repositories.Abstracts
+{
+    public interface IRepository<T> where T: class
+    {
+        T GetFirstOrDefault(Expression<Func<T, bool>> expression, string? includeProperties = null);
+        IEnumerable<T> GetAll(Expression<Func<T, bool>>? filter = null, string? includeProperties = null);
+        void Add(T entity);
+        void RemoveRange(IEnumerable<T> entities);
+    }
+}
