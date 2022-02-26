cf36c58dfa58e2f605576aec3fc8a6e5b10ec734
eabasquliyev <elgun.abasquliyev2@gmail.com>
2022-02-26 22:55:06 +0400

    Add IEmployeeRepository interface

---
+++ VacationPortal.DataAccess/Repositories/Abstracts/IEmployeeRepository.cs
@@ -0,0 +1,9 @@
+﻿using VacationPortal.Models;
+
+namespace VacationPortal.DataAccess.Repositories.Abstracts
+{
+    public interface IEmployeeRepository : IRepository<Employee>
+    {
+        void Update(Employee entity);
+    } 
+}
