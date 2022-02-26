fbdc8d32cd743261d5bbaf8a9d8ccb4850c4b655
eabasquliyev <elgun.abasquliyev2@gmail.com>
2022-02-26 22:19:10 +0400

    Add IncludeProperties extension method to IQueryable

---
+++ VacationPortal.DataAccess/Extensions/QueryableExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+namespace VacationPortal.DataAccess.Extensions
+{
+    public static class QueryableExtensions
+    {
+        public static IQueryable<T> IncludeProperties<T>(this IQueryable<T> query, string includeProperties) where T : class
+        {
+            var properties = includeProperties.Split(',', StringSplitOptions.RemoveEmptyEntries);
+            foreach (var property in properties)
+            {
+                query = query.Include(property);
+
+            }
+
+            return query;
+        }
+    }
+}
