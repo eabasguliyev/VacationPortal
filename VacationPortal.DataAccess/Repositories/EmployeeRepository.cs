f073657b278b25044d6734027dd33a9185b3bafb
eabasquliyev <elgun.abasquliyev2@gmail.com>
2022-02-26 22:56:31 +0400

    Implement IEmployeeRepository interface

---
+++ VacationPortal.DataAccess/Repositories/EmployeeRepository.cs
@@ -0,0 +1,18 @@
+﻿using VacationPortal.DataAccess.Data;
+using VacationPortal.DataAccess.Repositories.Abstracts;
+using VacationPortal.Models;
+
+namespace VacationPortal.DataAccess.Repositories
+{
+    public class EmployeeRepository : Repository<Employee>, IEmployeeRepository
+    {
+        public EmployeeRepository(ApplicationDbContext dbContext) : base(dbContext)
+        {
+        }
+
+        public void Update(Employee entity)
+        {
+            _dbSet.Update(entity);
+        }
+    }
+}
