6820cdeb0d9a89e4f3b08ec15a70b3a847b9f4e5
eabasquliyev <elgun.abasquliyev2@gmail.com>
2022-02-26 22:56:49 +0400

    Implement IPositionRepository interface

---
+++ VacationPortal.DataAccess/Repositories/PositionRepository.cs
@@ -0,0 +1,18 @@
+﻿using VacationPortal.DataAccess.Data;
+using VacationPortal.DataAccess.Repositories.Abstracts;
+using VacationPortal.Models;
+
+namespace VacationPortal.DataAccess.Repositories
+{
+    public class PositionRepository : Repository<Position>, IPositionRepository
+    {
+        public PositionRepository(ApplicationDbContext dbContext) : base(dbContext)
+        {
+        }
+
+        public void Update(Position entity)
+        {
+            _dbSet.Update(entity);
+        }
+    }
+}
