3e7d0662a6d0706b51a18058477c4019656e1c0f
eabasquliyev <elgun.abasquliyev2@gmail.com>
2022-02-26 22:25:52 +0400

    Add IUnitOfWork interface

---
+++ VacationPortal.DataAccess/Repositories/Abstracts/IUnitOfWork.cs
@@ -0,0 +1,9 @@
+﻿namespace VacationPortal.DataAccess.Repositories.Abstracts
+{
+    public interface IUnitOfWork
+    {
+        IDepartmentRepository DepartmentRepository { get; }
+
+        void Save();
+    }
+}
