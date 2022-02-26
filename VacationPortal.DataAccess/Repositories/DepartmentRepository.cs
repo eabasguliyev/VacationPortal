ff62a31317a1ef723857d11f7baf6877a2323ba8
eabasquliyev <elgun.abasquliyev2@gmail.com>
2022-02-26 22:24:03 +0400

    Implement IDepartmentRepository interface

---
+++ VacationPortal.DataAccess/Repositories/DepartmentRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VacationPortal.DataAccess.Data;
+using VacationPortal.DataAccess.Repositories.Abstracts;
+using VacationPortal.Models;
+
+namespace VacationPortal.DataAccess.Repositories
+{
+    public class DepartmentRepository : Repository<Department>, IDepartmentRepository
+    {
+        public DepartmentRepository(ApplicationDbContext dbContext) : base(dbContext)
+        {
+        }
+
+        public void Update(Department entity)
+        {
+            _dbSet.Update(entity);
+        }
+    }
+}
