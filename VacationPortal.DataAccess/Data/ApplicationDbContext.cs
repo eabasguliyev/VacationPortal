c1b374094265b4d7317bd6156f3d25a8c6ae828d
eabasquliyev <elgun.abasquliyev2@gmail.com>
2022-02-27 01:11:46 +0400

    Inherit ApplicationDbContext from IdentityDbContext

--- VacationPortal.DataAccess/Data/ApplicationDbContext.cs
+++ VacationPortal.DataAccess/Data/ApplicationDbContext.cs
@@ -1,16 +1,23 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using VacationPortal.DataAccess.Data.EntityConfigurations;
 using VacationPortal.Models;
 
 namespace VacationPortal.DataAccess.Data
 {
-    public class ApplicationDbContext:DbContext
+    public class ApplicationDbContext:IdentityDbContext<User, UserRole, int>
     {
         public DbSet<Department> Departments { get; set; }
+        public DbSet<Position> Positions { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<VacationApplication> VacationApplications { get; set; }
+        public DbSet<VacationInfo> VacationInfos { get; set; }
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
         {
 
         }
+
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
