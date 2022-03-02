881adb7bca05f569850ee95d47160218f610404c
eabasquliyev <elgun.abasquliyev2@gmail.com>
2022-03-02 16:17:39 +0400

    Add homecontroller

---
+++ VacationPortal.Web/Areas/Client/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VacationPortal.Web.Areas.Client.Controllers
+{
+    [Area("Client")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return RedirectToAction("Index", "VacationInfo");
+        }
+    }
+}
