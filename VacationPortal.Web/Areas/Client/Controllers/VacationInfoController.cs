ca9d27f10e2749f0fec615b5c013eb3cba6b837d
eabasquliyev <elgun.abasquliyev2@gmail.com>
2022-03-02 16:06:45 +0400

    Add Client Area

---
+++ VacationPortal.Web/Areas/Client/Controllers/VacationInfoController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VacationPortal.DataAccess.Repositories.Abstracts;
+
+namespace VacationPortal.Web.Areas.Client.Controllers
+{
+    public class VacationInfoController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public VacationInfoController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
