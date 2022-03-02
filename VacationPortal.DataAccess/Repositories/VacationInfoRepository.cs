4f5395eb10a5548820d9ab44557cc39de9e7c8da
eabasquliyev <elgun.abasquliyev2@gmail.com>
2022-03-02 16:06:29 +0400

    Implement IVacationInfoRepository interface

---
+++ VacationPortal.DataAccess/Repositories/VacationInfoRepository.cs
@@ -0,0 +1,18 @@
+﻿using VacationPortal.DataAccess.Data;
+using VacationPortal.DataAccess.Repositories.Abstracts;
+using VacationPortal.Models;
+
+namespace VacationPortal.DataAccess.Repositories
+{
+    public class VacationInfoRepository : Repository<VacationInfo>, IVacationInfoRepository
+    {
+        public VacationInfoRepository(ApplicationDbContext dbContext) : base(dbContext)
+        {
+        }
+
+        public void Update(VacationInfo entity)
+        {
+            _dbSet.Update(entity);
+        }
+    }
+}
