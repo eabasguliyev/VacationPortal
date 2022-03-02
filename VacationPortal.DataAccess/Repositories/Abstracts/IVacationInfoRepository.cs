e5d63f97b13bad4f584580ea74ed6510e7d36408
eabasquliyev <elgun.abasquliyev2@gmail.com>
2022-03-02 16:06:08 +0400

    Add IVacationInfoRepository interface

---
+++ VacationPortal.DataAccess/Repositories/Abstracts/IVacationInfoRepository.cs
@@ -0,0 +1,9 @@
+﻿using VacationPortal.Models;
+
+namespace VacationPortal.DataAccess.Repositories.Abstracts
+{
+    public interface IVacationInfoRepository : IRepository<VacationInfo>
+    {
+        void Update(VacationInfo entity);
+    } 
+}
